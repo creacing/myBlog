77fca6711df83a489fa07ae8de5d82c68c1e7a88
creazing <1538851062@qq.com>
2022-03-08 21:17:19 +0800

    ''

---
+++ .vitepress/config.ts
@@ -0,0 +1,77 @@
+import { getPosts, getPostLength } from "./theme/serverUtils";
+
+async function config() {
+  return {
+    lang: "en-US",
+    title: "ooo",
+    description: "ooo •ᴗ• blog",
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          type: "image/jpeg",
+          href: "/avator.jpg",
+        },
+      ],
+      [
+        "meta",
+        {
+          name: "author",
+          content: "ooo",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:title",
+          content: "Home",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:description",
+          content: "ooo •ᴗ• blog",
+        },
+      ],
+    ],
+    themeConfig: {
+      // repo: "clark-cui/homeSite",
+      logo: "/tea.svg",
+      docsDir: "/",
+      // docsBranch: "master",
+      lastUpdated: false,
+      posts: await getPosts(),
+      pageSize: 5, //几个为一页
+      postLength: await getPostLength(), //博客有几篇
+
+      //       algolia: {
+      //         apiKey: "90a0bae6ff7307fb76896cbe2f975b0c",
+      //         indexName: "clark-cui-docs",
+      //       },
+
+      nav: [
+        {
+          text: "🏡Home",
+          link: "/",
+        },
+        {
+          text: "🔖Tags",
+          link: "/tags",
+        },
+        {
+          text: "📃Archives",
+          link: "/archives",
+        },
+      ],
+      //侧边栏
+      // sidebar: {
+      //   "./posts/": false,
+      //   "/": false,
+      // },
+      sidebar: false,
+    },
+  };
+}
+export default config();
---
+++ .vitepress/theme/components/CopyWright.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    Powered by
+    <!-- <a href="https://github.com/clark-cui/" target="_blank" title="作者"
+      >clark-cui</a -->
+    <a href="" target="_blank" title="作者">ooo</a>
+    | Copyright © 2022-{{ date }} | MIT License
+  </footer>
+</template>
+<script lang="ts" setup>
+const date = new Date().getFullYear();
+</script>
+<style scoped>
+footer {
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  bottom: 20px;
+  padding: 0 20px;
+  background: var(--c-bg);
+  color: var(--c-text);
+}
+a {
+  color: var(--c-brand);
+  font-weight: bold;
+}
+a:hover {
+  text-decoration: none;
+  color: var(--c-hover);
+}
+</style>
+
