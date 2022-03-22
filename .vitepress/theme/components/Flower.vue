5e36b26ab1f083a5528c85e0a0c59623c95ae3f0
creazing <1538851062@qq.com>
2022-03-22 08:52:58 +0800

    animation

--- .vitepress/theme/components/Flower.vue
+++ .vitepress/theme/components/Flower.vue
@@ -43,97 +43,97 @@
 .div1 {
   /* z-index: -1; */
   /* 动画名称 持续时间 时间函数 延迟时间 循环次数 动画方向 播放装态 填充模式 */
-  animation: a1 8s ease 1 forwards;
+  animation: a1 2s ease 1 forwards;
 }
 .div2 {
   /* z-index: 2; */
   /* 动画名称 持续时间 时间函数 延迟时间 循环次数 动画方向 播放装态 填充模式 */
-  animation: a2 8s ease 1 forwards;
+  animation: a2 2s ease 1 forwards;
 }
 
 .div3 {
   /* z-index: 3; */
-  animation: a3 8s ease 1 forwards;
+  animation: a3 2s ease 1 forwards;
 }
 
 .div4 {
   /* z-index: 4; */
-  animation: a4 8s ease 1 forwards;
+  animation: a4 2s ease 1 forwards;
 }
 
 .div5 {
   /* z-index: 5; */
-  animation: a5 8s ease 1 forwards;
+  animation: a5 2s ease 1 forwards;
 }
 
 .div6 {
   /* z-index: 6; */
-  animation: a6 8s ease 1 forwards;
+  animation: a6 2s ease 1 forwards;
 }
 
 .div7 {
   /* z-index: 7; */
-  animation: a7 8s ease 1 forwards;
+  animation: a7 2s ease 1 forwards;
 }
 
 .div8 {
   /* z-index: 8; */
-  animation: a8 8s ease 1 forwards;
+  animation: a8 2s ease 1 forwards;
 }
 
 .div9 {
   /* z-index: 9; */
-  animation: a9 8s ease 1 forwards;
+  animation: a9 2s ease 1 forwards;
 }
 
 .div10 {
   /* z-index: 10; */
-  animation: a10 8s ease 1 forwards;
+  animation: a10 2s ease 1 forwards;
 }
 
 .div11 {
   /* z-index: 11; */
-  animation: a11 8s ease 1 forwards;
+  animation: a11 2s ease 1 forwards;
 }
 
 .div12 {
   /* z-index: 12; */
-  animation: a12 8s ease 1 forwards;
+  animation: a12 2s ease 1 forwards;
 }
 
 .div13 {
   /* z-index: 13; */
-  animation: a13 8s ease 1 forwards;
+  animation: a13 2s ease 1 forwards;
 }
 
 .div14 {
   /* z-index: 14; */
-  animation: a14 8s ease 1 forwards;
+  animation: a14 2s ease 1 forwards;
 }
 
 .div15 {
   /* z-index: 15; */
-  animation: a15 8s ease 1 forwards;
+  animation: a15 2s ease 1 forwards;
 }
 
 .div16 {
   /* z-index: 16; */
-  animation: a16 8s ease 1 forwards;
+  animation: a16 2s ease 1 forwards;
 }
 
 .div17 {
   /* z-index: 17; */
-  animation: a17 8s ease 1 forwards;
+  animation: a17 2s ease 1 forwards;
 }
 
 .div18 {
   /* z-index: 18; */
-  animation: a18 8s ease 1 forwards;
+  animation: a18 2s ease 1 forwards;
 }
 
 @keyframes a1 {
   from {
-    opacity: 0.7;
+    opacity: 0.1;
     z-index: -1;
   }
   to {
@@ -144,8 +144,8 @@
 
 @keyframes a2 {
   from {
-    opacity: 0.7;
-    z-index: 2;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -157,8 +157,8 @@
 
 @keyframes a3 {
   from {
-    opacity: 0.7;
-    z-index: 3;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -170,8 +170,8 @@
 
 @keyframes a4 {
   from {
-    opacity: 0.7;
-    z-index: 4;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -183,8 +183,8 @@
 
 @keyframes a5 {
   from {
-    opacity: 0.7;
-    z-index: 5;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -196,8 +196,8 @@
 
 @keyframes a6 {
   from {
-    opacity: 0.7;
-    z-index: 6;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -209,8 +209,8 @@
 
 @keyframes a7 {
   from {
-    opacity: 0.7;
-    z-index: 7;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -222,8 +222,8 @@
 
 @keyframes a8 {
   from {
-    opacity: 0.7;
-    z-index: 8;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -235,8 +235,8 @@
 
 @keyframes a9 {
   from {
-    opacity: 0.7;
-    z-index: 9;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -248,8 +248,8 @@
 
 @keyframes a10 {
   from {
-    opacity: 0.7;
-    z-index: 10;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -261,8 +261,8 @@
 
 @keyframes a11 {
   from {
-    opacity: 0.7;
-    z-index: 11;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -274,8 +274,8 @@
 
 @keyframes a12 {
   from {
-    opacity: 0.7;
-    z-index: 12;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -287,8 +287,8 @@
 
 @keyframes a13 {
   from {
-    opacity: 0.7;
-    z-index: 13;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -300,8 +300,8 @@
 
 @keyframes a14 {
   from {
-    opacity: 0.7;
-    z-index: 14;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -313,8 +313,8 @@
 
 @keyframes a15 {
   from {
-    opacity: 0.7;
-    z-index: 15;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -326,8 +326,8 @@
 
 @keyframes a16 {
   from {
-    opacity: 0.7;
-    z-index: 16;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -339,8 +339,8 @@
 
 @keyframes a17 {
   from {
-    opacity: 0.7;
-    z-index: 17;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
@@ -352,8 +352,8 @@
 
 @keyframes a18 {
   from {
-    opacity: 0.7;
-    z-index: 18;
+    opacity: 0.1;
+    z-index: -1;
     transform: rotate(-45deg);
   }
   to {
